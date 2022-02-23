93c3f83dee646a2b66959f2c13691f6c4eb61e7e
SPD3 <sean@ayerdoyles.org>
2022-02-23 18:59:40 -0500

    Cleaned up Features over indicies in preparation for the merge.

--- plotting/FeaturesOverIndices.py
+++ plotting/FeaturesOverIndices.py
@@ -19,17 +19,22 @@ from io import BytesIO
 
 class FeaturesOverIndices(ComparePlotsBase1):
 
-    def display_plots(self, yFeatureCreator:FeatureCreatorBase, points:Points) -> None:
+    BOTTOM_CROP = 63
+    TOP_CROP = 49
+    LEFT_CROP = 77
+    RIGHT_CROP = 60
+
+    def display_plots(self, yFeatureCreator:FeatureCreatorBase, points:Points, imageFile:str) -> None:
         """Displays a plot of the yFeatureCreator over time"""
-        #fig = plt.figure(figsize=(16, 7.5))
         
         ax_scatter = plt.axes()
-        #ax_scatter.tick_params(direction='in', top=True, right=True)
         
         tFeatureCreator = TFeatureCreator()
         xPoints = tFeatureCreator.get_features(points)
         yPoints = yFeatureCreator.get_features(points)
 
+        # This code is here, because the first and last two points are removed
+        # from Alina's plotting software
         xPoints = xPoints[1:len(xPoints)]
         yPoints = yPoints[1:len(yPoints)]
         del xPoints[-2::]
@@ -42,8 +47,6 @@ class FeaturesOverIndices(ComparePlotsBase1):
         if plottingNormally:
             plt.plot(xPoints, yPoints, color="red")
 
-            #ax_scatter.set_xlim(min(xPoints),max(xPoints))
-            #ax_scatter.set_ylim(min(yPoints),max(yPoints))
             ax_scatter.set_yscale('log')
             ax_scatter.set_xscale('log')
             ax_scatter.set_zorder(2)
@@ -54,16 +57,13 @@ class FeaturesOverIndices(ComparePlotsBase1):
             ax_tw_x = ax_scatter.twinx()
             ax_tw_x.axis('off')
             ax2 = ax_tw_x.twiny()
-            img = svg2png(file_obj=open('image.svg', "rb"))
+            img = svg2png(file_obj=open(imageFile, "rb"))
             im = Image.open(BytesIO(img))
             width, height = im.size
-            bottomAdjustment = 63
-            top = 49
-            left = 77
-            rightAdjustment = 60
+            
             print("width: ", width)
             print("height: ", height)
-            im = im.crop((left, top, width - rightAdjustment, height - bottomAdjustment))
+            im = im.crop((FeaturesOverIndices.LEFT_CROP, FeaturesOverIndices.TOP_CROP, width - FeaturesOverIndices.RIGHT_CROP, height - FeaturesOverIndices.BOTTOM_CROP))
 
             ax2.imshow(im, extent=[min(xPoints), max(xPoints), min(yPoints), max(yPoints)], aspect='auto')
             ax2.axis('off')
