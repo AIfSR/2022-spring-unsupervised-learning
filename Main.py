281a445fc589e86fa29a8ba41ec2fc1a875f60e5
SPD3 <sean@ayerdoyles.org>
2022-04-02 10:12:42 -0400

    New comments in main and moved things around to make them more readable.

--- Main.py
+++ Main.py
@@ -12,32 +12,38 @@ from sklearn.model_selection import cross_val_score
 from standardizefeaturesnumber.Extract40ValsRegularInterval import Extract40ValsRegularInterval
 from datasetfeatures.SyntheticMSDFeatures import SyntheticMSDFeatures
 
-
+# Main is the main module that loads in the features of the dataset, trains
+# a model on those features and reports the accuracy of that model.
 if __name__ == "__main__":
-    dataset = SyntheticDataset()
-    categories = dataset.getCategoriesWithPoints()
-
+    
+    # datasetFeatures defines the dataset and features that will be used for training
+    datasetFeatures = SyntheticMSDFeatures()
+    # normalizeFeatures defines the way that the features loaded in will be 
+    # normalized prior to being input into the algorithm.
     normalizeFeatures = DoNothingNormalization()
+    # standardizeFeatures defines the way that the length of the each example's 
+    # features will be standardized to ensure that the algorithm can process them
     standardizeFeatures = Extract40ValsRegularInterval()
-    featureCreator = ThreeDMSDFeatureCreator()
+    # algorithm defines the type of algorithm to be trained
     algorithm = LogisticRegression()
 
-    syntheticMSDFeatures = SyntheticMSDFeatures()
+    loadedLabels = datasetFeatures.getLabels()
+    loaded_dataSet = datasetFeatures.getDatasetOfFeatures()
 
-    loadedLabels = syntheticMSDFeatures.getLabels()
-    
-    loaded_dataSet = syntheticMSDFeatures.getDatasetOfFeatures()
-
+    # dataset is normalized and standardized
     dataSet = normalizeFeatures.normalizeToSetOfFeatures(loaded_dataSet)
-    dataSet = standardizeFeatures.standardizeSetOfFeatures(loaded_dataSet)
+    dataSet = standardizeFeatures.standardizeSetOfFeatures(dataSet)
+
+    # Sets up the train, test, and cross validation sets
     X_train, X_rem, y_train, y_rem = train_test_split(dataSet, loadedLabels, train_size=0.6, random_state = 1)
     test_size = 0.5
     X_valid, X_test, y_valid, y_test = train_test_split(X_rem, y_rem, test_size=0.5,random_state = 1)
 
     algorithm.train(X_train, y_train)
-    test_result = algorithm.predict(X_test)
-    train_result = algorithm.predict(X_train)
-    valid_result = algorithm.predict(X_valid)
+
+    train_prediction = algorithm.predict(X_train)
+    test_prediction = algorithm.predict(X_test)
+    valid_prediction = algorithm.predict(X_valid)
 
     yTrain = []
     for i in y_train:
@@ -72,6 +78,6 @@ if __name__ == "__main__":
         elif i == [0.0, 0.0, 0.0, 1.0]:
             yValid.append(4)
 
-    print("Training Accuracy:", metrics.accuracy_score(yTrain, train_result))
-    print("Test Accuracy:", metrics.accuracy_score(yTest, test_result))
-    print("Validation Accuracy:", metrics.accuracy_score(yValid, valid_result))
+    print("Training Accuracy:", metrics.accuracy_score(yTrain, train_prediction))
+    print("Test Accuracy:", metrics.accuracy_score(yTest, test_prediction))
+    print("Validation Accuracy:", metrics.accuracy_score(yValid, valid_prediction))
