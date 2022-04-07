a1c0eb299c96f0f743944f8a9d37211401040eb7
SPD3 <sean@ayerdoyles.org>
2022-04-07 17:24:39 -0400

    Split out the code in main into its own file to allow it to be imported into other modules

---
+++ src/diffusion_prediction/PredictDiffusionTypes.py
@@ -0,0 +1,88 @@
+import numpy as np
+import sys
+import os
+from features.Features import Features
+from ml_pipelines.LRPipelineFactory import LRPipelineFactory
+from ml_pipelines.MLPipelineBase import MLPipelineBase
+from standardizefeaturesnumber.FeatureStandardizationError import FeatureStandardizationError
+from tckfilereader.Points import Points
+from tckfilereader.PointsWithNames import PointsWithNames
+from tckfilereader.TCKFileReader import TCKFileReader
+
+def checkDirectory(directory:str) -> None:
+    directory = sys.argv[1]
+    if(not os.path.isdir(directory)):
+        raise IOError("Cannot find directory: " + directory)
+
+def checkFileIsValid(fileName:str) -> bool:
+    """Checks to ensure that a string is a valid file path and that it is a tck 
+    file"""
+    if not os.path.isfile(fileName):
+        return False
+    if not fileName.split(".")[-1] == "tck":
+        return False
+    return True
+
+def getTrajectories(inputTrajectoriesDirectory:str) -> list[Points]:
+    """Gets all of the trajectories as Points objects found within the 
+    inputTrajectoriesDirectory directory"""
+    points = []
+    tckFileReader = TCKFileReader()
+    for filename in os.listdir(inputTrajectoriesDirectory):
+        f = os.path.join(inputTrajectoriesDirectory, filename)
+        if(not checkFileIsValid(f)):
+            print("Could not read file: ", f)
+            continue
+        points.append(tckFileReader.get_points_with_name(f, False))
+    if len(points) == 0:
+        raise IOError("Could not read any files in: " + inputTrajectoriesDirectory)
+    return points
+
+def getFeaturesForAlgorithm(trajectories:list[PointsWithNames], mlPipeline:MLPipelineBase) -> list[Features]:
+    # The featureCreator converts points of a trajectory to features
+    featureCreator = mlPipeline.getFeatureCreator()
+    # The normalizeFeatures normalizes the feature values to ensure that feature 
+    # values passed into an algorithm are in an acceptable range.
+    normalizeFeatures = mlPipeline.getFeatureNormalizer()
+    # The standardizeFeatures standardized the number of features that the 
+    # algorithm takes to ensure that each occurance has a set number of features
+    standardizeFeatures = mlPipeline.getFeatureStandardizer()
+
+    allFeatures = []
+    for trajectory in trajectories:
+        try:
+            features = featureCreator.get_features(trajectory)
+            features = normalizeFeatures.normalizeFeature(features)
+            features = standardizeFeatures.standardizeFeatures(features)
+            allFeatures.append(features)
+        except:
+            print("Could not process file: ")
+            print(trajectory.getName())
+            print("Does this trajectory have enough points? (more than 50 x,y,z,t points)")
+            print()
+        
+    return allFeatures
+
+# predict is the main method that reads in the trajectories, calculates the MSD 
+# values for them, predicts their diffusion types, and then prints out these 
+# predictions
+def predict(inputTrajectoriesDirectory:str):
+
+    # This is the concrete factory class that defines the feature creator, 
+    # normalizer, standardizer, and algorithm being used to process the 
+    # trajectories for the Logistic Regression algorithm
+
+    checkDirectory(inputTrajectoriesDirectory)
+
+    mlPipeline = LRPipelineFactory()
+
+    algorithm = mlPipeline.getAlgorithm()
+    
+    points = getTrajectories(inputTrajectoriesDirectory)
+    inputFeatures = getFeaturesForAlgorithm(points, mlPipeline)
+
+    predictions = algorithm.predict(inputFeatures)
+
+    for name, prediction in predictions:
+        print(name + ": " + str(prediction))
+    
--- src/diffusion_prediction/Utilities.py
+++ src/diffusion_prediction/Utilities.py
@@ -9,7 +9,7 @@ _mainDirectory = _mainDirectory.split("/")
 indexOfDir = _mainDirectory.index(DIR_NAME)
 _mainDirectory = _mainDirectory[0:indexOfDir+1]
 _mainDirectory = "/".join(_mainDirectory)
-
+_mainDirectory += "/src/diffusion_prediction"
 def getMainDirectory():
     return _mainDirectory
 
