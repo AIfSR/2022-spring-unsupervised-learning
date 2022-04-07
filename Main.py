da1a48be9934ee45a650af4f70c699f13eb80c7b
SPD3 <sean@ayerdoyles.org>
2022-04-06 20:16:47 -0400

    Got rid of unnecessary Comments and code in Main

--- Main.py
+++ Main.py
@@ -2,21 +2,16 @@ import numpy as np
 import sys
 import os
 from algorithms.LogisticRegression import LogisticRegression
-from sklearn.model_selection import train_test_split
 from features.ThreeDMSDFeatureCreator import ThreeDMSDFeatureCreator
 from normalizefeatures.ScaletoMillion import ScaletoMillion
 from standardizefeaturesnumber.Extract40ValsRegularInterval import Extract40ValsRegularInterval
-from datasetfeatures.SyntheticMSDFeatures import SyntheticMSDFeatures
-from tckfilereader.PointsWithNames import PointsWithNames
 from tckfilereader.TCKFileReader import TCKFileReader
 
 # Main is the main module that loads in the features of the dataset, trains
 # a model on those features and reports the accuracy of that model.
 if __name__ == "__main__":
-    featureCreator = ThreeDMSDFeatureCreator()
     
-    # datasetFeatures defines the dataset and features that will be used for training
-    datasetFeatures = SyntheticMSDFeatures()
+    featureCreator = ThreeDMSDFeatureCreator()
     # normalizeFeatures defines the way that the features loaded in will be 
     # normalized prior to being input into the algorithm.
     normalizeFeatures = ScaletoMillion()
@@ -26,22 +21,6 @@ if __name__ == "__main__":
     # algorithm defines the type of algorithm to be trained
     algorithm = LogisticRegression()
 
-    # loadedLabels = datasetFeatures.getLabels()
-    # loaded_dataSet = datasetFeatures.getDatasetOfFeatures()
-
-    # # dataset is normalized and standardized
-    # dataSet = normalizeFeatures.normalizeToSetOfFeatures(loaded_dataSet)
-    # dataSet = standardizeFeatures.standardizeSetOfFeatures(dataSet)
-
-    # # Sets up the train, test, and cross validation sets
-    # X_train, X_rem, y_train, y_rem = train_test_split(dataSet, loadedLabels, train_size=0.6, random_state = 1)
-    # test_size = 0.5
-    # X_valid, X_test, y_valid, y_test = train_test_split(X_rem, y_rem, test_size=0.5,random_state = 1)
-    # print("Starting training")
-    # algorithm.train(X_train, y_train)
-    # print("Done training")
-    # algorithm.save("algorithms/", "LR_V1.0")
-    
     algorithm.load("algorithms/LR_V1.0.pkl")
     inputTrajectoriesDirectory = sys.argv[1]
     inputPoints = []
