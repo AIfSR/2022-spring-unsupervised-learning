5f8db88353cc19aedbddd53cc7e626d9fffcc966
SPD3 <sean@ayerdoyles.org>
2022-04-08 08:24:25 -0400

    Excluded the test files from the package distribution

--- build/lib/tests/featurestests/FeaturesWithNamestest.py
+++
@@ -1,8 +0,0 @@
-import unittest
-from diffusion_prediction.features.FeaturesWithNames import FeaturesWithNames
-
-class FeaturesWithNamesTest (unittest.TestCase):
-
-    def test_getName(self):
-        featuresWithNames = FeaturesWithNames("name")
-        self.assertEquals(featuresWithNames.getName(), "name")
