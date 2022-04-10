50551e21ce44201733168964159561efa9a313b8
SPD3 <sean@ayerdoyles.org>
2022-04-10 16:28:42 -0400

    New comments in build

--- build/lib/AIfSR_Trajectory_Analysis/Trajectory_Analysis.py
+++ build/lib/AIfSR_Trajectory_Analysis/Trajectory_Analysis.py
@@ -1,13 +1,30 @@
 import AIfSR_Trajectory_Analysis.PredictDiffusionTypes as PredictDiffusionTypes
 
-# predict is the main method that reads in the trajectories, calculates the MSD 
-# values for them, predicts their diffusion types, and then prints out these 
-# predictions
 def predict(inputTrajectoriesDirectory:str):
-    # This is the concrete factory class that defines the feature creator, 
-    # normalizer, standardizer, and algorithm being used to process the 
-    # trajectories for the Logistic Regression algorithm
+    """This is the function used to predict the diffusion types of trajectories 
+    using the most accurate machine learning model within this build. Pass in 
+    the path to a directory as a string and every trajectory within that 
+    directory will be analyzed. Trajectories are files that end in .tck and 
+    contain x,y,z,t points in the following format:
+    line 1: x points, seperated by spaces
+    line 2: y points, seperated by spaces
+    line 3: z points, seperated by spaces
+    line 4: t points, seperated by spaces
+    This function will print out the file names of the trajectories it predicts 
+    and then the type(s) of diffusion that it detects within those trajectories
 
+    Example usage (Assume /Users/seandoyle/MyDirectory is a directory containing 
+    the trajectories: Trajectory1.tck, Trajectory2.tck, and Trajectory3.tck):
+    >>> predict("/Users/seandoyle/MyDirectory")
+    /Users/seandoyle/MyDirectory/Trajectory1.tck:
+    Ballistic: No, Confined Diffusion: No, Random Walk: Yes, Very Confined Diffusion: No
+
+    /Users/seandoyle/MyDirectory/Trajectory2.tck:
+    Ballistic: No, Confined Diffusion: No, Random Walk: No, Very Confined Diffusion: Yes
+
+    /Users/seandoyle/MyDirectory/Trajectory3.tck:
+    Ballistic: No, Confined Diffusion: Yes, Random Walk: No, Very Confined Diffusion: No
+    """
     PredictDiffusionTypes.checkDirectory(inputTrajectoriesDirectory)
 
     mlPipeline = PredictDiffusionTypes.LRPipelineFactory()
