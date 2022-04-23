0c1a70017a89d2291db542c35f47ec05a8dc81c9
SPD3 <sean@ayerdoyles.org>
2022-04-23 14:38:41 -0400

    Deleted build directory

--- build/lib/AIfSR_Trajectory_Analysis/normalizefeatures/DoNothingNormalization.py
+++
@@ -1,9 +0,0 @@
-
-from AIfSR_Trajectory_Analysis.normalizefeatures.NormalizeFeaturesBase import NormalizeFeaturesBase
-from AIfSR_Trajectory_Analysis.features.Features import Features
-
-class DoNothingNormalization (NormalizeFeaturesBase):
-    """Does nothing to the Dataset to normalize it."""
-
-    def normalizeToSetOfFeatures(self, setOfFeatures:list[Features]) -> list[Features]:
-        return setOfFeatures
--- build/lib/AIfSR_Trajectory_Analysis/standardizefeaturesnumber/FeatureStandardizationError.py
+++
@@ -1,5 +0,0 @@
-
-
-class FeatureStandardizationError (Exception):
-    def __str__(self) -> str:
-        return super().__str__() + "Problem Standardizing Feature"
